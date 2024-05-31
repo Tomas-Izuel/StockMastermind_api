17323e48be00b0dc6406fb419066f49a3c3a9f41
Tomas <tomasizuel@gmail.com>
2024-05-31 19:47:31 -0300

    delete default controller & service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { OrderStatusModule } from './modules/order-status.module';
 import { ArticleModule } from './modules/article.module';
 import { FamilyModule } from './modules/family.module';
@@ -8,7 +6,5 @@ import { SaleStatusModule } from './modules/sale-status.module';
 
 @Module({
   imports: [FamilyModule, ArticleModule, SaleStatusModule, OrderStatusModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
