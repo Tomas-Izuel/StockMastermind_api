723db399b931f74db3e55fa3dbfbc6ea8309bd29
DarioLopez18 <darioangellopez38@gmail.com>
2024-06-03 19:41:30 -0300

    creando orden

--- src/dtos/demand-params.dto.ts
+++ src/dtos/demand-params.dto.ts
@@ -1,4 +1,4 @@
-import { IsNumber, IsString } from "class-validator";
+import { IsNumber, IsOptional, IsString } from "class-validator";
 
 
 export class CreateDemandParamDto {
@@ -10,5 +10,19 @@ export class CreateDemandParamDto {
         allowNaN: false,
         allowInfinity: false,
     })
+    aceptable_error: number;
+}
+
+export class EditDemandParamDto {
+    @IsString({
+        message: 'El método de solución debe ser un string.'
+    })
+    @IsOptional()
+    method: string;
+    @IsNumber({
+        allowNaN: false,
+        allowInfinity: false,
+    })
+    @IsOptional()
     aceptable_error: number;
 }
