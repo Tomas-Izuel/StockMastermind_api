de8a6450ab0c033b32cfd3384e3ec7b8e7b70096
DarioLopez18 <darioangellopez38@gmail.com>
2024-06-03 19:56:01 -0300

    agregando getUpdate

--- src/controllers/discount-ship-cost.controller.ts
+++ src/controllers/discount-ship-cost.controller.ts
@@ -15,4 +15,31 @@ import { DiscountShipCostService } from 'src/services/discount-ship-cost.service
 @Controller('discount-ship-cost')
 export class DiscountShipCostController {
   constructor(private discountShipCostService: DiscountShipCostService) { }
+
+  @Get()
+  async getDiscountShipCost() {
+    return await this.discountShipCostService.getDiscountShipCost();
+  }
+
+  @Get(':id')
+  async getDiscountShipCostById(@Param('id') id: number) {
+    return await this.discountShipCostService.getDiscountShipCostByProviderId(id);
+  }
+
+  @Post()
+  @UsePipes(new ValidationPipe({ transform: true }))
+  async createDiscountShipCost(@Body() body: any) {
+    return await this.discountShipCostService.createDiscountShipCost(body);
+  }
+
+  @Put(':id')
+  @UsePipes(new ValidationPipe({ transform: true }))
+  async updateDiscountShipCost(@Param('id') id: number, @Body() body: any) {
+    return await this.discountShipCostService.updateDiscountShipCost(id, body);
+  }
+
+  @Delete(':id')
+  async deleteDiscountShipCost(@Param('id') id: number) {
+    return await this.discountShipCostService.deleteDiscountShipCost(id);
+  }
 }
