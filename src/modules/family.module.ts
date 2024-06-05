73d11e792b761bb3265095f41a565d75fb13985a
DarioLopez18 <darioangellopez38@gmail.com>
2024-06-05 14:59:46 -0300

    pero la queria tanto

---
+++ src/modules/family.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { FamilyController } from 'src/controllers/family.controller';
+import { PrismaService } from 'src/lib/prisma/prisma.service';
+import { FamilyService } from 'src/services/family.service';
+
+@Module({
+  controllers: [FamilyController],
+  providers: [FamilyService, PrismaService],
+})
+export class FamilyModule { }
